b9dee24f6a76dc1460b4c32d38aa4bbc0f7b033a
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-05-19 18:22:10 +0800

    [added] brute force

--- src/pointSET.ts
+++ src/pointSET.ts
@@ -2,13 +2,53 @@ import Point2D from "./doNotTouch/point2D";
 import RectHV from "./doNotTouch/rectHV";
 
 class PointSET {
-    public constructor() {} // construct an empty set of points
-    public isEmpty(): boolean {} // is the set empty?
-    public size(): number {} // number of points in the set
-    public insert(p: Point2D): void {} // add the point to the set (if it is not already in the set)
-    public contains(p: Point2D): boolean {} // does the set contain point p?
-    public draw(p): void {} // draw all points to p5
-    public range(rect: RectHV): Point2D[] {} // all points that are inside the rectangle (or on the boundary)
+    points: Point2D[]
+
+    public constructor() {
+        this.points = []
+    }
+
+    public isEmpty(): boolean {
+        if (this.points.length === 0) {
+            return true
+        }
+        return false
+    }
+
+    public size(): number {
+        return this.points.length
+    }
+    public insert(p: Point2D): void {
+        if (!this.contains(p)) {
+            this.points = [...this.points, p]
+        }
+    
+    } // add the point to the set (if it is not already in the set)
+
+    public contains(p: Point2D): boolean {
+        this.points.forEach(point => {
+            if (point.equals(p)) {
+                return true
+            }
+        })
+        return false
+    }
+
+    public draw(p): void {
+        p.stroke(0);
+        p.strokeWeight(.01);
+        console.log(this.points)
+
+        for (const point of this.points) {
+            p.point(point.x, point.y);
+        }
+    }
+
+    public range(rect: RectHV): Point2D[] {
+        return this.points.filter(point =>
+            rect.contains(point)
+        );
+    }
 }
 
 export default PointSET;
